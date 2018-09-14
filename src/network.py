8f94648d2b13fe0e6cb5a690e6c6d6a3775d1e45
Christopher King <Chris.D.King.42@gmail.com>
2018-09-14 13:23:10 -0400

    Full Matrix Based

--- src/network.py
+++ src/network.py
@@ -73,8 +73,10 @@ class Network(object):
         is the learning rate."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+        xs = np.array([x for x, y in mini_batches])
+        ys = np.array([y for x, y in mini_batches])
+        delta_nabla_bs, delta_nabla_ws = self.backprop(xs, ys)
+        for delta_nabla_b, delta_nabla_w in zip(delta_nabla_bs, delta_nabla_ws):
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         self.weights = [w-(eta/len(mini_batch))*nw
@@ -82,7 +84,7 @@ class Network(object):
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
 
-    def backprop(self, x, y):
+    def backprop(self, xs, ys):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
@@ -90,19 +92,19 @@ class Network(object):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
-        activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activation = xs
+        activations = [xs] # list to store all the activations, layer by layer
+        zss = [] # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
-            zs.append(z)
-            activation = sigmoid(z)
+            zs = np.dot(w, activation)+b
+            zss.append(zs)
+            activation = sigmoid(zs)
             activations.append(activation)
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
+        deltas = self.cost_derivative(activations[-1], ys) * \
             sigmoid_prime(zs[-1])
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        nabla_bs[-1] = deltas
+        nabla_ws[-1] = np.dot(deltas, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
         # differently to the notation in Chapter 2 of the book.  Here,
         # l = 1 means the last layer of neurons, l = 2 is the
@@ -110,12 +112,12 @@ class Network(object):
         # scheme in the book, used here to take advantage of the fact
         # that Python can use negative indices in lists.
         for l in xrange(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+            zs = zss[-l]
+            sps = sigmoid_prime(zs)
+            deltas = np.dot(self.weights[-l+1].transpose(), deltas) * sps
+            nabla_bs[-l] = deltas
+            nabla_ws[-l] = np.dot(deltas, activations[-l-1].transpose())
+        return (nabla_bs, nabla_ws)
 
     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
